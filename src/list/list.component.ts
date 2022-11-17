ae9cb280824667e8b59d323a7d1d13c1e96ae17d
shashidhar0902 <shashidhar.gandi@gmail.com>
2022-11-17 16:03:33 +0530

    1 file modified

--- src/list/list.component.ts
+++ src/list/list.component.ts
@@ -3,38 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
   showImage: boolean = false;
-  products:any[] = [
+  products: any[] = [
     {
-      'ProductId':1,
-      'ProductName': 'One',
-      'productImageUrl': './../assets/Images/One.jpg'
+      ProductId: 1,
+      ProductName: 'One',
+      productImageUrl: '../../assets/Images/One.jpg',
     },
     {
-      'ProductId':2,
-      'ProductName': 'Two',
-      'productImageUrl': './../assets/Images/Two.jpg'
-
+      ProductId: 2,
+      ProductName: 'Two',
+      productImageUrl: '../../assets/Images/Two.jpg',
     },
     {
-      'ProductId':3,
-      'ProductName': 'Three',
-      'productImageUrl': './../assets/Images/Three.jpg'
-
+      ProductId: 3,
+      ProductName: 'Three',
+      productImageUrl: '../../assets/Images/Three.jpg',
     },
     {
-      'ProductId':4,
-      'ProductName': 'Four',
-      'productImageUrl': './../assets/Images/Four.jpg'
+      ProductId: 4,
+      ProductName: 'Four',
+      productImageUrl: '../../assets/Images/Four.jpg',
+    },
+  ];
 
-    }
-]
-
-toggleImage(){
-  this.showImage = !this.showImage;
-}
- 
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
 }
