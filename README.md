0ff815a5745fb2f1c9625310075731aee9731546
shashidhar0902 <shashidhar.gandi@gmail.com>
2022-11-17 15:57:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-new
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ewuhlz)
---
+++ src/list/list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  showImage: boolean = false;
+  products:any[] = [
+    {
+      'ProductId':1,
+      'ProductName': 'One',
+      'productImageUrl': './../assets/Images/One.jpg'
+    },
+    {
+      'ProductId':2,
+      'ProductName': 'Two',
+      'productImageUrl': './../assets/Images/Two.jpg'
+
+    },
+    {
+      'ProductId':3,
+      'ProductName': 'Three',
+      'productImageUrl': './../assets/Images/Three.jpg'
+
+    },
+    {
+      'ProductId':4,
+      'ProductName': 'Four',
+      'productImageUrl': './../assets/Images/Four.jpg'
+
+    }
+]
+
+toggleImage(){
+  this.showImage = !this.showImage;
+}
+ 
+}
